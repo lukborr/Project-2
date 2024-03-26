242d9e0199ab26b6fa02241fecfb2ab30a08d898
lukborr <lukborr@gmail.com>
2024-03-26 22:43:18 +0100

    exp bar done

--- project_2-main/Assets/Scripts/Experience.cs
+++ project_2-main/Assets/Scripts/Experience.cs
@@ -1,31 +1,39 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Experience : MonoBehaviour
 {
     private int characterExperience = 0;
-    private int characterLevel;
+    private int characterLevel = 1;
     private float requiredExpToLvlUp = 3;
     private int exp = 3;
     [SerializeField] GameObject rewardMenu;
+    [SerializeField] Image expImage;
+    [SerializeField] TextMeshProUGUI levelText;
+
+    private void Start()
+    {
+        expImage.fillAmount = 0;
+    }
 
     private void Update()
     {
-           if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             GainExperience(1);
+            Debug.Log($"exp: {characterExperience}");
         }
     }
     public void GainExperience(int experience)
     {
         characterExperience += experience;
-        
-        if(characterExperience >= exp)
+        if (characterExperience >= exp)
         {
             LevelUp();
-        }    
+        }
+        ChangeExpBar();
     }
     private void OnEnable()
     {
@@ -41,29 +49,37 @@ public class Experience : MonoBehaviour
     {
         EventManager.CallLeveledUpEvent();
         characterLevel++;
-        characterExperience= 0;
+        characterExperience = 0;
         requiredExpToLvlUp = requiredExpToLvlUp + requiredExpToLvlUp * 0.33f;
         exp = (int)requiredExpToLvlUp;
-
-        
+        ChangeLevelText();
     }
 
     private void OpenUpgradeWindow(List<string> skills)
     {
         rewardMenu.SetActive(true);
-        if(rewardMenu.GetComponent<RewardMenu>() != null )
+        if (rewardMenu.GetComponent<RewardMenu>() != null)
         {
             RewardMenu rewardMenuScript = rewardMenu.GetComponent<RewardMenu>();
             List<OffensiveSkillSO> skillsSo = new List<OffensiveSkillSO>();
-            for(int i = 0; i < skills.Count; i++)
+            for (int i = 0; i < skills.Count; i++)
             {
                 OffensiveSkillSO skillSO = Resources.Load<OffensiveSkillSO>("SO/SkillsSO/" + skills[i]);
                 Debug.Log(skillSO.skillName);
                 skillsSo.Add(skillSO);
             }
             rewardMenuScript.DrawSkills(skillsSo);
-        
         }
-        
     }
+
+    private void ChangeExpBar()
+    {
+        expImage.fillAmount = characterExperience / requiredExpToLvlUp;
+    }
+
+    private void ChangeLevelText()
+    {
+        levelText.text = $"Level: {characterLevel.ToString()}";
+    }
+
 }
